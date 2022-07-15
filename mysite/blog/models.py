8c92b339470efc00c14f17bc3a195e100ab1e540
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-07-15 18:13:25 -0400

    Hasta url

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,14 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self): 
+        return super(PublishedManager, self).get_queryset().filter(status="published")
+
+
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ("draft", "Draft"),
@@ -18,9 +26,13 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
 
+    objects = models.Manager()
+    published = PublishedManager()   
+
 class Meta:
     # Indica que cuando se realice una consulta, por defecto ordene en base a "publish" en orden descendiente
     ordering = ("-publish")
 
 def __str__(self):
     return self.title
+
